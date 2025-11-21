90566f2bdfefbb509f2f9405a415f99ecc9a4ada
dbakushkin <dany93@mail.ru>
2025-11-21 11:52:01 +0300

    fix

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ let way = process.argv[4];
 const myEmitter = new eventEmitter();
 
 myEmitter.on("plus", (a, b) => {
-  myEmitter.emit("result", plus(a, b));
+  myEmitter.emit("result", plus(+a, +b));
 });
 
 myEmitter.on("divide", (a, b) => {
-  myEmitter.emit("result", minus(a, b));
+  myEmitter.emit("result", divide(+a, +b));
 });
 
 myEmitter.on("minus", (a, b) => {
-  myEmitter.emit("result", multiply(a, b));
+  myEmitter.emit("result", minus(+a, +b));
 });
 
 myEmitter.on("multiply", (a, b) => {
-  myEmitter.emit("result", divide(a, b));
+  myEmitter.emit("result", multiply(+a, +b));
 });
 
 myEmitter.on("result", (data) => {
--- multiply.js
+++ multiply.js
@@ -1,3 +1,3 @@
 module.exports = function multiply(a, b) {
-  console.log(a * b);
+  return a * b;
 };
