5d3647c531305c152bdf61bd389a843311455a15
dbakushkin <dany93@mail.ru>
2025-11-21 11:47:26 +0300

    fix

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const eventEmitter = require("events");
+const plus = require("./plus");
+const minus = require("./minus");
+const multiply = require("./multiply");
+const divide = require("./divide");
 
 let firstNumber = process.argv[2];
 let secondNumber = process.argv[3];
@@ -7,19 +11,19 @@ let way = process.argv[4];
 const myEmitter = new eventEmitter();
 
 myEmitter.on("plus", (a, b) => {
-  myEmitter.emit("result", a + b);
+  myEmitter.emit("result", plus(a, b));
 });
 
 myEmitter.on("divide", (a, b) => {
-  myEmitter.emit("result", a / b);
+  myEmitter.emit("result", minus(a, b));
 });
 
 myEmitter.on("minus", (a, b) => {
-  myEmitter.emit("result", a - b);
+  myEmitter.emit("result", multiply(a, b));
 });
 
 myEmitter.on("multiply", (a, b) => {
-  myEmitter.emit("result", a * b);
+  myEmitter.emit("result", divide(a, b));
 });
 
 myEmitter.on("result", (data) => {
